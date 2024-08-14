fa02fab67e21e4bbe37e0fdf888576ccf3b6ba40
pkrwlgns64@gmail.com <pkrwlgns64@gmail.com>
2024-08-14 10:20:58 +0900

    20240812-1020

---
+++ react03/day03/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+
+var componets = document.getElementById('root');
+var root = ReactDOM.createRoot(componets);
+root.render(< App/>)
