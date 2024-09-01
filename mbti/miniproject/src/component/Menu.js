0141ea31c1bba21bac4900b07964877f288d09ec
jihoon <pkrwlgns64@gmail.com>
2024-09-01 13:32:17 +0900

    수정

--- mbti/miniproject/src/component/Menu.js
+++ mbti/miniproject/src/component/Menu.js
@@ -1,4 +1,8 @@
+import {Link,Outlet} from "react-router-dom";
 const Menu = () => {
-  return <></>;
+  return(<div>
+    <Link to="/">Home</Link>
+    <Link to="login"></Link>
+  </div>) ;
 };
 export default Menu();
